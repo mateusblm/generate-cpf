51741ee6f137bb88015d10ca4da3260f49f4d630
MateusBurlamaqui <mateusburla@gmail.com>
2020-10-11 16:02:35 -0500

    Update generate_cpf.py

--- generate_cpf.py
+++ generate_cpf.py
@@ -1,27 +1,43 @@
 from random import randint  
+import time
 start = input("Wanna generate a CPF? Type Yes:  ")
-if start == "Yes":
-    cpf = ''.join([str(randint(0, 9)) for x in range(9)])
-    new_cpf = cpf
-    reverse = 10
-    total = 0
+count = 0
+storage = []
+while count < 11
+    if start == "Yes":
+        cpf = ''.join([str(randint(0, 9)) for x in range(9)])
+        new_cpf = cpf
+        reverse = 10
+        total = 0
 
-    for i in range(19):
-        if i > 8:
-            i -= 9
+        for i in range(19):
+            if i > 8:
+                i -= 9
 
-        total += int(new_cpf[i]) * reverse
+            total += int(new_cpf[i]) * reverse
 
-        reverse -= 1
-        if reverse < 2:
-            reverse = 11
-            d = 11 - (total % 11)
+            reverse -= 1
+            if reverse < 2:
+                reverse = 11
+                d = 11 - (total % 11)
 
-            if d > 9:
-                d = 0
-            total = 0
-            new_cpf += str(d)
-print(new_cpf)
+                if d > 9:
+                    d = 0
+                total = 0
+                new_cpf += str(d)
+    storage.append(new_cpf)
+    count += 1
+             
+        
+def random(n):
+    for cpfs in n:
+        yield cpfs
+        time.sleep(0.1)
+        
+        
+s = random(storage)
+for c in s:
+    print(c)
 
 """
 Copyright (c) 2020 Mateus Burlamaqui
